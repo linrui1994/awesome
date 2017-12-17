e6a29b51913d64d150f533156b7abb0208b5996a
linrui <linrui@gridsum.com>
2017-12-17 12:35:09 +0800

    update push.js

--- push.js
+++ push.js
@@ -3,6 +3,8 @@
 require('shelljs/global')
 var cmd = exec('git symbolic-ref --short HEAD')
 var message = process.argv[2]
+var current = process.argv[3]
+
 if (cmd.stderr) {
   exit(1)
 }
@@ -11,26 +13,31 @@ if (!message) {
   exit(1)
 }
 var branch = cmd.stdout.replace(/\r?\n/, '')
-if (branch === 'develop' || branch === 'master') {
+if ((branch === 'develop' || branch === 'master') && current !== 'current') {
   console.log('Do not directly modify the master or develop branch')
   exit(1)
 }
 
+message = JSON.stringify(message);
+
 exec(`git add .`)
 exec(`git commit -m ${message}`)
 
-var checkout = exec('git checkout develop')
-if (/(fatal)|(error)/.test(checkout.stderr + checkout.stdout)) {
-  exit(1)
+if (current === 'current') {
+  var checkout = exec('git checkout develop')
+  if (/(fatal)|(error)/.test(checkout.stderr + checkout.stdout)) {
+    exit(1)
+  }
+  var gitpull = exec('git pull origin develop')
+  if (/(fatal)|(error)/.test(gitpull.stderr + gitpull.stdout)) {
+    exit(1)
+  }
+  exec(`git checkout ${branch}`)
+  var rebase = exec('git rebase develop')
+  if (/(fatal)|(error)/.test(rebase.stdout + rebase.stderr)) {
+    exit(1)
+  }
 }
-var gitpull = exec('git pull origin develop')
-if (/(fatal)|(error)/.test(gitpull.stderr + gitpull.stdout)) {
-  exit(1)
-}
-exec(`git checkout ${branch}`)
-var rebase = exec('git rebase develop')
-if (/(fatal)|(error)/.test(rebase.stdout + rebase.stderr)) {
-  exit(1)
-}
+
 exec(`git push origin ${branch}`)
 console.log('done')
